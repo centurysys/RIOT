38f0a7b9db48bcb20e5155ade110ceaf4cff4476
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2015-07-24 13:02:02 +0900

    boards: mma-200: update USEMODULE.

--- boards/mma-200/include/board.h
+++ boards/mma-200/include/board.h
@@ -57,7 +57,7 @@ extern "C" {
 #define STDIO               UART_0
 #define STDIO_BAUDRATE      (115200U)
 #define STDIO_RX_BUFSIZE    (64U)
-#define STDIO_TX_BUFSIZE    (4096U * 2)
+#define STDIO_TX_BUFSIZE    (4096U * 3)
 /** @} */
 
 /**
